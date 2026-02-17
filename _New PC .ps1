d47fc304b2fce3cfe98eb7c9129874c8f0025dd0
Ilya <tkachily000@gmail.com>
2026-02-17 14:44:44 -0800

    Update _New PC .ps1
    remove packages from AppxProvisionedPackage

--- _New PC .ps1
+++ _New PC .ps1
@@ -16,45 +16,69 @@
 #Get-AppxPackage *QuickAssist* | Remove-AppxPackage
 #Get-AppxPackage *screensketch* | Remove-AppxPackage
 
-Get-AppxPackage *3dbuilder* | Remove-AppxPackage
-Get-AppxPackage *3dviewer* | Remove-AppxPackage
-Get-AppxPackage *alarms* | Remove-AppxPackage
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
-Get-AppxPackage *bingnews* | Remove-AppxPackage
-Get-AppxPackage *bingsearch* | Remove-AppxPackage
-Get-AppxPackage *bingsports* | Remove-AppxPackage
-Get-AppxPackage *bingweather* | Remove-AppxPackage
-Get-AppxPackage *clipchamp* | Remove-AppxPackage
-Get-AppxPackage *communications* | Remove-AppxPackage
-Get-AppxPackage *copilot* | Remove-AppxPackage
-Get-AppxPackage *devhome* | Remove-AppxPackage
-Get-AppxPackage *family* | Remove-AppxPackage
-Get-AppxPackage *feedback* | Remove-AppxPackage
-Get-AppxPackage *gamingapp* | Remove-AppxPackage
-Get-AppxPackage *gethelp* | Remove-AppxPackage
-Get-AppxPackage *getstarted* | Remove-AppxPackage
-Get-AppxPackage *heif* | Remove-AppxPackage
-Get-AppxPackage *maps* | Remove-AppxPackage
-Get-AppxPackage *mixedreality* | Remove-AppxPackage
-Get-AppxPackage *msteams* | Remove-AppxPackage
-Get-AppxPackage *officehub* | Remove-AppxPackage
-Get-AppxPackage *onedrive* | Remove-AppxPackage
-Get-AppxPackage *onenote* | Remove-AppxPackage
-Get-AppxPackage *outlook* | Remove-AppxPackage
-Get-AppxPackage *people* | Remove-AppxPackage
-Get-AppxPackage *phone* | Remove-AppxPackage
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
-Get-AppxPackage *soundrecorder* | Remove-AppxPackage
-Get-AppxPackage *StartExperiences* | Remove-AppxPackage
-Get-AppxPackage *storepurchase* | Remove-AppxPackage
-Get-AppxPackage *todo* | Remove-AppxPackage
-Get-AppxPackage *vp9* | Remove-AppxPackage
-Get-AppxPackage *wallet* | Remove-AppxPackage
-Get-AppxPackage *webpimage* | Remove-AppxPackage
-Get-AppxPackage *xbox* | Remove-AppxPackage
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage -Allusers
+Get-AppxPackage *3dviewer* | Remove-AppxPackage -Allusers
+Get-AppxPackage *alarms* | Remove-AppxPackage -Allusers
+Get-AppxPackage *bingfinance* | Remove-AppxPackage -Allusers
+Get-AppxPackage *bingnews* | Remove-AppxPackage -Allusers
+Get-AppxPackage *bingsearch* | Remove-AppxPackage -Allusers
+Get-AppxPackage *bingsports* | Remove-AppxPackage -Allusers
+Get-AppxPackage *bingweather* | Remove-AppxPackage -Allusers
+Get-AppxPackage *clipchamp* | Remove-AppxPackage -Allusers
+Get-AppxPackage *communications* | Remove-AppxPackage -Allusers
+Get-AppxPackage *copilot* | Remove-AppxPackage -Allusers
+Get-AppxPackage *devhome* | Remove-AppxPackage -Allusers
+Get-AppxPackage *family* | Remove-AppxPackage -Allusers
+Get-AppxPackage *feedback* | Remove-AppxPackage -Allusers
+Get-AppxPackage *gamingapp* | Remove-AppxPackage -Allusers
+Get-AppxPackage *getstarted* | Remove-AppxPackage -Allusers
+Get-AppxPackage *maps* | Remove-AppxPackage -Allusers
+Get-AppxPackage *mixedreality* | Remove-AppxPackage -Allusers
+Get-AppxPackage *msteams* | Remove-AppxPackage -Allusers
+Get-AppxPackage *officehub* | Remove-AppxPackage -Allusers
+Get-AppxPackage *onedrive* | Remove-AppxPackage -Allusers
+Get-AppxPackage *onenote* | Remove-AppxPackage -Allusers
+Get-AppxPackage *outlook* | Remove-AppxPackage -Allusers
+Get-AppxPackage *people* | Remove-AppxPackage -Allusers
+Get-AppxPackage *phone* | Remove-AppxPackage -Allusers
+Get-AppxPackage *skypeapp* | Remove-AppxPackage -Allusers
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage -Allusers
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage -Allusers
+Get-AppxPackage *StartExperiences* | Remove-AppxPackage -Allusers
+Get-AppxPackage *storepurchase* | Remove-AppxPackage -Allusers
+Get-AppxPackage *todo* | Remove-AppxPackage -Allusers
+Get-AppxPackage *wallet* | Remove-AppxPackage -Allusers
+Get-AppxPackage *xbox* | Remove-AppxPackage -Allusers
+Get-AppxPackage *zunemusic* | Remove-AppxPackage -Allusers
+Get-AppxPackage *zunevideo* | Remove-AppxPackage -Allusers
+
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*3dbuilder*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*3dviewer*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingfinance*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingnews*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingsearch*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingsports*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingweather*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*clipchamp*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*communications*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*copilot*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*devhome*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*family*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*gamingapp*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*getstarted*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*maps*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*mixedreality*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*msteams*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*officehub*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*onedrive*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*onenote*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*outlook*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*people*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*skypeapp*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*todo*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*wallet*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*zunemusic*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*zunevideo*' | Remove-AppxProvisionedPackage -Online
 
 winget uninstall OneDriveSetup.exe
 Disable-WindowsOptionalFeature -Online -FeatureName "Recall"
@@ -68,13 +92,12 @@ choco feature enable -n allowGlobalConfirmation
 choco install 7zip -y
 choco install notepadplusplus -y
 choco install chrome -y
-
-choco install vcredist-all -y
-choco install dotnet-6.0-runtime -y
 choco install firefoxesr -y
 
+#choco install vcredist-all -y
 #choco install eartrumpet -y
 #choco install hwinfo -y
 
 # Chris Titus Ultimate Windows Utility
 iwr -useb https://christitus.com/win | iex
+
