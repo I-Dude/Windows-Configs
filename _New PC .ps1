f78425c9daca7327d3adf23da35eacf3576453cd
Ilya <tkachily000@gmail.com>
2026-04-21 20:49:23 -0700

    Update _New PC .ps1

--- _New PC .ps1
+++ _New PC .ps1
@@ -1,5 +1,6 @@
 # https://www.digitalcitizen.life/how-remove-default-windows-10-apps-powershell-3-steps/
 # Get-AppxPackage | findstr FullName
+# Get-AppxProvisionedPackage -online | select DisplayName
 
 #Reinstall all default packages from AppXmanifest (all packages from default user)
 #Get-AppxPackage -Allusers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
@@ -15,9 +16,12 @@
 #Get-AppxPackage *photos* | Remove-AppxPackage
 #Get-AppxPackage *QuickAssist* | Remove-AppxPackage
 #Get-AppxPackage *screensketch* | Remove-AppxPackage
+#Get-AppxPackage *zunemusic* | Remove-AppxPackage -Allusers
 
 Get-AppxPackage *3dbuilder* | Remove-AppxPackage -Allusers
 Get-AppxPackage *3dviewer* | Remove-AppxPackage -Allusers
+Get-AppxPackage *549981C3F5F10* | Remove-AppxPackage -Allusers
+Get-AppxPackage *aimgr* | Remove-AppxPackage -Allusers
 Get-AppxPackage *alarms* | Remove-AppxPackage -Allusers
 Get-AppxPackage *bingfinance* | Remove-AppxPackage -Allusers
 Get-AppxPackage *bingnews* | Remove-AppxPackage -Allusers
@@ -27,6 +31,7 @@ Get-AppxPackage *bingweather* | Remove-AppxPackage -Allusers
 Get-AppxPackage *clipchamp* | Remove-AppxPackage -Allusers
 Get-AppxPackage *communications* | Remove-AppxPackage -Allusers
 Get-AppxPackage *copilot* | Remove-AppxPackage -Allusers
+Get-AppxPackage *crossdevice* | Remove-AppxPackage -Allusers
 Get-AppxPackage *devhome* | Remove-AppxPackage -Allusers
 Get-AppxPackage *family* | Remove-AppxPackage -Allusers
 Get-AppxPackage *feedback* | Remove-AppxPackage -Allusers
@@ -35,12 +40,13 @@ Get-AppxPackage *getstarted* | Remove-AppxPackage -Allusers
 Get-AppxPackage *maps* | Remove-AppxPackage -Allusers
 Get-AppxPackage *mixedreality* | Remove-AppxPackage -Allusers
 Get-AppxPackage *msteams* | Remove-AppxPackage -Allusers
-Get-AppxPackage *officehub* | Remove-AppxPackage -Allusers
+Get-AppxPackage *office* | Remove-AppxPackage -Allusers
 Get-AppxPackage *onedrive* | Remove-AppxPackage -Allusers
 Get-AppxPackage *onenote* | Remove-AppxPackage -Allusers
 Get-AppxPackage *outlook* | Remove-AppxPackage -Allusers
 Get-AppxPackage *people* | Remove-AppxPackage -Allusers
 Get-AppxPackage *phone* | Remove-AppxPackage -Allusers
+Get-AppxPackage *PowerAutomate* | Remove-AppxPackage -Allusers
 Get-AppxPackage *skypeapp* | Remove-AppxPackage -Allusers
 Get-AppxPackage *solitairecollection* | Remove-AppxPackage -Allusers
 Get-AppxPackage *soundrecorder* | Remove-AppxPackage -Allusers
@@ -49,11 +55,12 @@ Get-AppxPackage *storepurchase* | Remove-AppxPackage -Allusers
 Get-AppxPackage *todo* | Remove-AppxPackage -Allusers
 Get-AppxPackage *wallet* | Remove-AppxPackage -Allusers
 Get-AppxPackage *xbox* | Remove-AppxPackage -Allusers
-Get-AppxPackage *zunemusic* | Remove-AppxPackage -Allusers
 Get-AppxPackage *zunevideo* | Remove-AppxPackage -Allusers
 
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*3dbuilder*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*3dviewer*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*549981C3F5F10*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*aimgr*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingfinance*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingnews*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingsearch*' | Remove-AppxProvisionedPackage -Online
@@ -62,22 +69,27 @@ Get-AppxProvisionedPackage -Online | ? DisplayName -like '*bingweather*' | Remov
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*clipchamp*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*communications*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*copilot*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*crossdevice*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*devhome*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*family*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*feedback*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*gamingapp*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*getstarted*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*maps*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*mixedreality*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*msteams*' | Remove-AppxProvisionedPackage -Online
-Get-AppxProvisionedPackage -Online | ? DisplayName -like '*officehub*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*office*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*onedrive*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*onenote*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*outlook*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*people*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*phone*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*PowerAutomate*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*skypeapp*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*solitairecollection*' | Remove-AppxProvisionedPackage -Online
+Get-AppxProvisionedPackage -Online | ? DisplayName -like '*stickynotes*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*todo*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*wallet*' | Remove-AppxProvisionedPackage -Online
-Get-AppxProvisionedPackage -Online | ? DisplayName -like '*zunemusic*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*zunevideo*' | Remove-AppxProvisionedPackage -Online
 
 winget uninstall OneDriveSetup.exe
@@ -93,10 +105,13 @@ choco install 7zip -y
 choco install notepadplusplus -y
 choco install chrome -y
 choco install firefoxesr -y
+choco install vlc -y
 
 #choco install vcredist-all -y
 #choco install eartrumpet -y
 #choco install hwinfo -y
+#choco install powertoys
+
 
 # Chris Titus Ultimate Windows Utility
 iwr -useb https://christitus.com/win | iex
